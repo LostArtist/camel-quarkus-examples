e88f97ca278624f5e42df06f4fa1feced64b82a1
Nikita Konovalov <nkonoval@redhat.com>
2024-04-10 18:32:50 +0200

    Fixes:
    - Changed README.adoc
    - Added rest-assured dependency to pom.xml
    - Added quarkus test class
    - Code cleanups

--- artemis-elsaticsearch/src/test/java/org/acme/ElasticSearchTest.java
+++ artemis-elsaticsearch/src/test/java/org/acme/ElasticSearchTest.java
@@ -18,21 +18,13 @@
 package org.acme;
 
 import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.QuarkusTest;
 import org.acme.resource.CustomArtemisTestResource;
 import org.acme.resource.ElasticSearchTestResource;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-@QuarkusIntegrationTest
+@QuarkusTest
 @QuarkusTestResource(CustomArtemisTestResource.class)
 @QuarkusTestResource(ElasticSearchTestResource.class)
-public class ElasticSearchIndexITCase {
-
-    @Test
-    public void testSomeSampleStuff() {
-        // TODO add test
-        Assertions.assertTrue(true, "You need to add tests");
-    }
+public class ElasticSearchTest {
 
 }
