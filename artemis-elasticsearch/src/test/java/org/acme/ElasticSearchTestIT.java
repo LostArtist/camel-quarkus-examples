2bc8c8c0c8fbf3feaaffccde9d5cb7dc8391a073
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-04-18 11:42:47 +0200

    Adjusted the test resource to use Paho instead of JMS

--- artemis-elasticsearch/src/test/java/org/acme/ElasticSearchTestIT.java
+++ artemis-elasticsearch/src/test/java/org/acme/ElasticSearchTestIT.java
@@ -2,11 +2,11 @@ package org.acme;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import org.acme.resource.CustomArtemisTestResource;
+import org.acme.resource.CustomPahoTestResource;
 import org.acme.resource.ElasticSearchTestResource;
 
 @QuarkusIntegrationTest
-@QuarkusTestResource(CustomArtemisTestResource.class)
+@QuarkusTestResource(CustomPahoTestResource.class)
 @QuarkusTestResource(ElasticSearchTestResource.class)
 public class ElasticSearchTestIT extends ElasticSearchTest {
 }
